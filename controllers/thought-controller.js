9a99f2fba2173962b61432371ff22caa94d18094
HotBarbeque <122079759+HotBarbeque@users.noreply.github.com>
2023-07-01 18:01:31 -0500

    Complete thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,115 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+  // Retrieve all Thought documents from the database
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => {
+                res.status(400).json(err);
+        });
+    },
+  // Retrieve a single Thought document by its ID
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+        }
+        res.json(dbThoughtData);
+        })
+        .catch((err) => res.json(err));
+    },
+  // Create a new Thought document and associate it with a User document
+    createThought({ params, body }, res) {
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No user with this id' });
+                return;
+        }
+        res.json(dbThoughtData);
+        })
+        .catch((err) => res.status(400).json(err));
+    },
+  // Update an existing Thought document by its ID
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                    }
+                    res.json(dbThoughtData);
+            })
+        .catch((err) => {
+        res.status(400).json(err);
+        });
+    },
+  // Add a new reaction to an existing Thought document
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+ // Remove a reaction from an existing Thought document
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => res.json(err));
+    },
+  // Delete an existing Thought document by its ID and update the associated User document
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+            .then((dbThoughtData) => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id' });
+                    return;
+                }
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $pull: { thoughts: params.thoughtId } },
+                    { new: true }
+                );
+            })
+            .then((dbThoughtData) => {
+                console.log(dbThoughtData);
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'no user found with this id' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+};
+
+module.exports = thoughtController;
