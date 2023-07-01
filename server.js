31d2a08da754b3ba8b298e8092024ae195dcb167
HotBarbeque <122079759+HotBarbeque@users.noreply.github.com>
2023-07-01 17:42:30 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1/C18-Social-Network-API-KC',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+mongoose.set('strictQuery', true);
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(` Connected on localhost:${PORT}`));
