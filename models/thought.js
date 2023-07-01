3a009bd38f48665ba793637d1275dfcaa2d64aed
HotBarbeque <122079759+HotBarbeque@users.noreply.github.com>
2023-07-01 18:51:46 -0500

    Complete though.js model

---
+++ models/thought.js
@@ -0,0 +1,72 @@
+// Import required modules
+const { Schema, model, Types } = require('mongoose');
+
+// Define Reaction schema
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+
+// Define Thought schema
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        reactions: [ReactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+// Create a virtual property to count the number of reactions
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+// Create Thought model using Thought schema
+const Thought = model('Thought', ThoughtSchema);
+
+// Export Thought model
+module.exports = Thought;
